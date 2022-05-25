b122748746db98fa9ecf8dfde47f9bdee924cf22
amosbaby <931963081@qq.com>
2022-05-25 14:00:44 +0800

    turn square into function-component

--- src/tic-tac-toe/tic-tac-toe.js
+++ src/tic-tac-toe/tic-tac-toe.js
@@ -2,14 +2,15 @@ import React   from "react";
 
 import './tic-tac-toe.css'
 
-class Square extends React.Component {
-  render() {
+/** 
+ * 函数组件：不需要继承React.Component,接收一个props，仅需要返回一个render组件元素就行了。
+*/
+function Square(props) {
     return (
-      <button className="square" onClick={()=>this.props.onClick()}>
-        { this.props.value }
+      <button className="square" onClick={()=>props.onClick()}>
+        { props.value }
       </button>
     );
-  }
 }
 
 class Board extends React.Component {
@@ -17,8 +18,8 @@ class Board extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      squares:Array(9).fill(null),
-      step:0
+      squares: Array(9).fill(null),
+      step: 0 // 记录当前步数，判断下一步是X还是O
     }
   }
 
@@ -30,7 +31,7 @@ class Board extends React.Component {
     // 保证数据的不可变性
     const squares = this.state.squares.slice()
     if(squares[index] === null){
-      const step = this.state.step+1
+      const step = this.state.step + 1
       squares[index] = step % 2 ? 'X' : 'O'
       this.setState({
         step,squares
