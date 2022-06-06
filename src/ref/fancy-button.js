2185df0b08a95c0beb829996cae2945040982348
amosbaby <931963081@qq.com>
2022-06-06 22:08:26 +0800

    feat add HOC ref

--- src/ref/fancy-button.js
+++ src/ref/fancy-button.js
@@ -5,6 +5,41 @@ const FancyButton = React.forwardRef((props,ref)=>(
   </button>
 ))
 
+function logProps(WrapComponent){
+
+  class LogProps extends React.Component{
+
+    componentDidUpdate(preProps){
+      console.log('old props:',preProps)
+      console.log('new props:',this.props)
+    }
+
+    render(){
+      const {forwardRef,...rest} = this.props
+      console.log('forwardRef:',forwardRef)
+      return (
+        <WrapComponent ref={forwardRef} {...rest}></WrapComponent>
+      )
+    }
+
+  }
+
+  function forwardRef(props,ref){
+    return (
+      <LogProps {...props} forwardedRef={ref} />
+    )
+  }
+
+  const name = WrapComponent.displayName || WrapComponent.name || 'WrapComponent';
+  forwardRef.displayName = `logProps(${name})`;
+
+  // return LogProps
+  return React.forwardRef(forwardRef)
+
+}
+
+const WrapButton = logProps(FancyButton)
+
 
 export function TestFancyButton(){
 
@@ -13,8 +48,8 @@ export function TestFancyButton(){
     console.log(ref)
   }
   return (
-    <FancyButton ref={ref}>
+    <WrapButton ref={ref}>
       <div onClick={handleClick}> Hello Ref </div>
-    </FancyButton>
+    </WrapButton>
   )
 }
